804cfd3d40e5c810039c795e78aaa7d9f626a558
c-blomquist <blomquist.caleb@gmail.com>
2023-11-04 19:44:18 -0400

    Created local postgres database and implemented adding tasks and listing all tasks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+var pgp = require('pg-promise')();
+const db = pgp(/* Insert in the database connection here for use */);
 
-
-// TODO: create database connection with PostgreSQL
+// TODO: Setup remote database connection instead of just locally hosted
 // TODO: check for internet connection to be able to grab items from database
 
 if(process.argv.length < 3){
@@ -9,7 +10,7 @@ if(process.argv.length < 3){
 
 // lists all tasks and their status, example: node index.js list 
 if(process.argv[2] === "list"){
-    // TODO: add in call to database to get all list objects
+    getTasks();
 }
 
 // create a new task, example: node index.js create "This is my new task"
@@ -17,7 +18,10 @@ if(process.argv[2] === "create"){
     if(!process.argv[3]){
         console.error("Please enter a task name to create.")
     }
-    // TODO: add in call to post task to database.
+
+    // TODO: Should add in some sort of check that the task name being passed in works 
+
+    addTask(process.argv[3]);
 }
 
 if(process.argv[2] === "complete"){
@@ -27,4 +31,42 @@ if(process.argv[2] === "complete"){
     // TODO: add in call to post update to task to mark as completed
     // TODO: logic to not allow completing a task that doesnt exist
     // TODO: logic to not allow completing an already completed task
+}
+
+// Takes in task name for creation and outputs the id of the created task
+async function addTask(taskName) {
+    try {
+        const query = `INSERT INTO task (name, completed, createddate)
+                        VALUES ($1, false, NOW())
+                        RETURNING id;`;
+
+        const res = await db.one(query, [taskName]);
+        console.log(`Task added with the ID of: ${res.id}`);
+    }
+    catch (err) {
+        console.error("Error adding in task: " + err);
+    }
+    finally {
+        pgp.end();
+    }
+}
+
+async function getTasks() {
+    try {
+        const query = `SELECT id, name, completed FROM task;`;
+        const res = await db.manyOrNone(query);
+
+        if(res.length == 0) {
+            console.log("No tasks were found in database.");
+        }
+        else { 
+            res.forEach(task => {console.log(`ID: ${task.id}, Task name: ${task.name}, Completion status: ${task.completed}`);});
+        }
+    }
+    catch (err) {
+        console.error("Error being able to get tasks: " + err)
+    }
+    finally {
+        pgp.end();
+    }
 }
