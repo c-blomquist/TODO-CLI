007804950f7d0ee571e49d804a26cd4c58772151
Nick Winner <me@nickwinner.com>
2023-11-11 22:52:38 +0000

    test: add test coverage for getTasks:

--- index.test.js
+++ index.test.js
@@ -1,4 +1,22 @@
+const mockTestTask = {
+    id: 1,
+    name: 'Test Task',
+    completed: false,
+    createdData: new Date(),
+    completedDate: new Date()
+};
+
+jest.mock('./database', () => {
+    return {
+        db: {
+            manyOrNone: jest.fn().mockResolvedValue([mockTestTask])
+        },
+        end: jest.fn()
+    }
+});
+
 const server = require('./index.js');
+const { db } = require('./database');
 
 // TODO test on a testing database
 // TODO adding in another test causes it to fail as the connection to the datbase has been closed already.
@@ -7,12 +25,22 @@ describe('TODO CLI Tests', () => {
 
     test('Getting all tasks', async () => {
         const res = await server.getTasks();
-
         expect(res).toBeDefined();
+        expect(res).toEqual([mockTestTask])
     });
+
+    describe('Getting tasks fails', () => {
+        beforeAll(() => {
+            jest.mocked(db).manyOrNone.mockRejectedValueOnce(new Error('This is a test error!'));
+        })
+
+        test('should throw error', async () => {
+            await expect(() => server.getTasks()).rejects.toEqual(new Error('This is a test error!'))
+        })
+    })
 
     // TODO: Try to fix database connection to be able to run multiple tests
     // test('Inputting an empty name', async () => {
-    //     await expect(server.addTask(undefined)).rejects.toThrow("Error adding in task:");
+    //     await expect(server.addTask(undefined)).rejects.toThrow(new"Error adding in task:");
     // });
 });
