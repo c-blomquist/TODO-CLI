2c49e6d37c709e8a1c5bda0be166045c62cc3c92
c-blomquist <blomquist.caleb@gmail.com>
2023-12-22 16:18:09 -0500

    Removing extra awaits, and extra error messages

--- databaseFunctions.js
+++ databaseFunctions.js
@@ -1,45 +1,39 @@
 const { db, end } = require("./database.js");
 
 // Takes in task name for creation and outputs the id of the created task
-async function addTask(taskName) {
+function addTask(taskName) {
   try {
     const query = `INSERT INTO task (name, completed, created_date)
                         VALUES ($1, false, NOW())
                         RETURNING id;`;
 
-    const res = await db.one(query, [taskName]);
-    return res;
+    return db.one(query, [taskName]);
   } catch (err) {
-    console.error("Error adding in task: " + err);
     throw err;
   } finally {
     end();
   }
 }
 
-async function getTasks() {
+function getTasks() {
   try {
     const query = `SELECT id, name, completed 
                         FROM task;`;
-    const res = await db.manyOrNone(query);
-    return res;
+    return db.manyOrNone(query);
   } catch (err) {
-    console.error("Error being able to get tasks: " + err);
     throw err;
   } finally {
     end();
   }
 }
 
-async function getTask(taskID) {
+function getTask(taskID) {
   try {
     const query = `SELECT id, completed
                         FROM task
                         WHERE id = $1;`;
-    const res = await db.one(query, [taskID]);
-    return res;
+    return db.one(query, [taskID]);
   } catch (err) {
-    console.error("Error getting a task by ID: " + err);
     throw err;
   }
 }
@@ -48,17 +42,14 @@ async function completeTask(taskID) {
   try {
     const task = await getTask(taskID);
     if (task.completed == true) {
-      console.error("Task is already completed!");
       throw new Error("Task is already completed!");
     }
     const query = `UPDATE task
                         SET completed = true, completed_date = NOW()
                         WHERE id = $1
                         RETURNING name, completed, completed_date;`;
-    const res = await db.one(query, [taskID]);
-    return res;
+    return db.one(query, [taskID]);
   } catch (err) {
-    console.error("Error being able to complete task: " + err);
     throw err;
   } finally {
     end();
